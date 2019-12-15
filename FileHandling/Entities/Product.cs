ac58627c209ee97bfb9b5abf71fec6369b694fbf
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2019-12-15 19:54:11 -0300

    Finished

---
+++ FileHandling/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileHandling
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        public Product(string name, double price, int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+
+        public double Total()
+        {
+            return Price * Amount;
+        }
+    }
+}
